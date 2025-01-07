30402523720f9c7cd935077d645a645ab8c105cb
Anastasiya Makarova <asyalapa@mail.ru>
2025-01-08 02:37:05 +0300

    style: add style for initial game screen

--- simon-says/script.js
+++ simon-says/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let startBtn = document.querySelector('.btn');
+
+startBtn.addEventListener('mousemove', (event) => {
+  let x = event.pageX - startBtn.offsetLeft;
+  let y = event.pageY - startBtn.offsetTop;
+  startBtn.style.setProperty('--x', x + 'px');
+  startBtn.style.setProperty('--y', y + 'px');
+});
